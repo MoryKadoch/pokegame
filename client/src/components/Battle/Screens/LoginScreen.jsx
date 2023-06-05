e280ee236ba277e32f526fac6b1f4f20ce270ffc
MoryKadoch <moryykadoch@gmail.com>
2023-06-05 08:34:45 +0200

    fix

--- client/src/components/Battle/Screens/LoginScreen.jsx
+++ client/src/components/Battle/Screens/LoginScreen.jsx
@@ -24,17 +24,21 @@ const LoginScreen = () => {
                         Login to play
                     </Typography>
                     <Box display="flex" justifyContent="center" width="100%" marginBottom={2} style={{ padding: '0 16px' }}>
-                        <TextField
-                            id="username"
-                            label="Username"
-                            variant="outlined"
-                            value={username}
-                            onChange={(e) => setUsername(e.target.value)}
-                            style={{ marginTop: 16, marginBottom: 16, width: '100%' }}
-                        />
-                        <Button onClick={handleLogin} variant="contained" color="primary" style={{ width: '100%' }} size="large">
-                            Login
-                        </Button>
+                        <Box width="100%" marginBottom={2}>
+                            <TextField
+                                id="username"
+                                label="Username"
+                                variant="outlined"
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                                style={{ marginTop: 16, marginBottom: 16, width: '100%' }}
+                            />
+                        </Box>
+                        <Box width="100%">
+                            <Button onClick={handleLogin} variant="contained" color="primary" style={{ width: '100%' }} size="large">
+                                Login
+                            </Button>
+                        </Box>
                     </Box>
                 </Box>
             </Grid>
